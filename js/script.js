ca0b3e6461630ca3c215451fad4eafbde836bb81
denispianelli <denis.pianelli@gmail.com>
2023-04-24 19:41:20 +0200

    Create functions for basic math operators

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function add(...numbers) {
+  const res = numbers.reduce((num1, num2) => num1 + num2);
+  return res;
+}
+
+function subtract(...numbers) {
+  const res = numbers.reduce((num1, num2) => num1 - num2);
+  return res;
+}
+
+function multiply(...numbers) {
+  const res = numbers.reduce((num1, num2) => num1 * num2);
+  return res;
+}
+
+function divide(...numbers) {
+  const res = numbers.reduce((num1, num2) => num1 / num2);
+  return res;
+}
+
